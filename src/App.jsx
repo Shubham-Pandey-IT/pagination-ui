be9e2dd0916179afcb0c15a4cafbd695f65407d3
Shubham-Pandey-IT <shubhamp7282@gmail.com>
2026-03-23 11:24:34 +0530

    Making of Pagination and its concept

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Pagination from './Components/Pagination'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Pagination/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Pagination.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { useState } from "react";
+
+const Pagination = () => {
+  const mockData = [];
+
+  for (let i = 1; i <= 30; i++) {
+    mockData.push({
+      id: i,
+      title: `Card ${i}`,
+      desc: "This is a sample description about page  " + i,
+    });
+  }
+
+  const ITEMS_PER_PAGE = 6;
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(mockData.length / ITEMS_PER_PAGE);
+
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const currentData = mockData.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+
+  // Pagination buttons
+
+  const buttons = [];
+  for (let i = 1; i <= totalPages; i++) {
+    buttons.push(
+      <button
+        key={i}
+        className={currentPage === i ? "active" : ""}
+        onClick={() => setCurrentPage(i)}
+      >
+        {i}
+      </button>,
+    );
+  }
+
+  return (
+    <div className="container">
+      <div className="card-container">
+        {currentData.map((item) => (
+          <div className="card" key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.desc}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="pagination-buttons">
+        <button
+          onClick={() => setCurrentPage((p) => p - 1)}
+          disabled={currentPage === 1}
+        >
+          Prev
+        </button>
+
+        {buttons}
+
+        <button
+          onClick={() => setCurrentPage((p) => p + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
